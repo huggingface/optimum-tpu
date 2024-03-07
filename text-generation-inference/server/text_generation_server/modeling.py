60ad09ce79a31155b2d2730ad12c1112445d1556
Alvaro Moran <alvaro.moran@huggingface.co>
2024-03-07 17:23:00 +0000

    debug: added env var to debug on CPU
    if DBG_DEVICE env var is set, it will used to set the device for the
    model.

--- text-generation-inference/server/text_generation_server/modeling.py
+++ text-generation-inference/server/text_generation_server/modeling.py
@@ -19,7 +19,6 @@ from os import PathLike, environ
 from typing import Any
 
 import torch
-import torch_xla.core.xla_model as xm
 from loguru import logger
 from transformers import AutoModelForCausalLM
 from transformers.utils import is_accelerate_available
@@ -42,7 +41,11 @@ class TpuModelForCausalLM(AutoModelForCausalLM):
         if "PJRT_DEVICE" not in environ:
             logger.info("PJRT_DEVICE environment variable not found. Setting it to 'TPU'.")
             environ["PJRT_DEVICE"] = "TPU"
-        device = xm.xla_device()
+        if "DBG_DEVICE" in environ:
+            device = environ["DBG_DEVICE"]
+            logger.debug(f"Device set to: {device}")
+        else:
+            device = "xla"
         if is_accelerate_available():
             model = AutoModelForCausalLM.from_pretrained(
                 pretrained_model_name_or_path, device_map=device, *model_args, **kwargs
@@ -62,6 +65,7 @@ class TpuModelForCausalLM(AutoModelForCausalLM):
 
         # Do eval, and compile
         model.eval()
-        model = torch.compile(model, backend="openxla_eval")
+        if device == "xla":
+            model = torch.compile(model, backend="openxla_eval")
 
         return model
