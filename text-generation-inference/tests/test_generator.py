be1194bef6ba30edc9063f7492dfb7830eb9ec50
Alvaro Moran <alvaro.moran@huggingface.co>
2024-03-14 14:48:25 +0000

    fix(CI): adapt expected result in do_sample test
    The test produces different results after some operations are being done
    in a slightly different order.

--- text-generation-inference/tests/test_generator.py
+++ text-generation-inference/tests/test_generator.py
@@ -109,7 +109,7 @@ def test_prefill(input_text, token_id, token_text, do_sample, batch_size, model_
         [
             "It was a bright cold day in April, and the clocks were striking thirteen.",
             20,
-            " We sat outside the house, drinking coffee, listening to the traffic. And then, suddenly, we",
+            " We sat outside the house, drinking coffee, listening to the orchestra playing through the window. We could",
             True,
         ],
     ],
