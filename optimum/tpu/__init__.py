2b673f2510f950743bdae22e6c79bc6d58a49e5b
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-04-08 10:07:45 +0200

    Make AutoModelForX top-level import

--- optimum/tpu/__init__.py
+++ optimum/tpu/__init__.py
@@ -12,4 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from .modeling import AutoModelForCausalLM
 from .version import __version__, VERSION  # noqa: F401
