cd992264a856bb5fbc9670df5e158179d914899b
Alvaro Moran <alvaro.moran@huggingface.co>
2024-04-09 16:23:51 +0000

    feat: model il loaded using pytorch_dtype from config
    This will lead to loading the model in bfloat16 when specified in the
    config.

--- optimum/tpu/distributed_model.py
+++ optimum/tpu/distributed_model.py
@@ -12,7 +12,7 @@ import torch_xla.distributed.xla_multiprocessing as xmp
 import torch.multiprocessing as mp
 
 from optimum.tpu.modeling import AutoModelForCausalLM
-from transformers import PretrainedConfig
+from transformers import PretrainedConfig, AutoConfig
 
 
 class ModelCommand(Enum):
@@ -90,7 +90,8 @@ def _mp_fn(rank, model_id, root_mailbox: RootMailbox, sample_fn: callable):
     )
 
     # Model loading and sharding should happen here
-    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float32)
+    config = AutoConfig.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=config.torch_dtype)
     model = model.eval()
     model.to(device)
     if rank == 0:
