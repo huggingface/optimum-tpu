2eb609e9689b0cc4de3b2e1f9b6c1c1ae42cca8d
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-04-17 14:32:36 +0200

    Expose match_batch_size as envvar for TGI entrypoint

--- text-generation-inference/entrypoint.sh
+++ text-generation-inference/entrypoint.sh
@@ -22,6 +22,12 @@ else
   export TGI_MAX_CONCURRENT_REQUESTS 4
 fi
 
+if [[ -n "${TGI_MAX_BATCH_SIZE}" ]]; then
+  export TGI_MAX_BATCH_SIZE="${TGI_MAX_BATCH_SIZE}"
+else
+  export TGI_MAX_BATCH_SIZE 1
+fi
+
 if [[ -n "${TGI_MAX_INPUT_LENGTH}" ]]; then
   export TGI_MAX_INPUT_LENGTH="${TGI_MAX_INPUT_LENGTH}"
 else
@@ -48,6 +54,7 @@ fi
 
 text-generation-launcher --port 8080 \
   --max-concurrent-requests ${TGI_MAX_CONCURRENT_REQUESTS}
+  --max-batch-size ${TGI_MAX_BATCH_SIZE}
   --max-input-length ${TGI_MAX_INPUT_LENGTH} \
   --max-total-tokens ${TGI_MAX_TOTAL_TOKENS} \
   --max-batch-prefill-tokens ${TGI_MAX_BATCH_PREFILL_TOKENS} \
