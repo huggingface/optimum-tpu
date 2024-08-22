1b83380fe11a1184fdf2e476357044816c2537c7
wenxindongwork <wenxindong@google.com>
2024-08-22 08:06:10 -0700

    make style

--- optimum/tpu/fsdp_v2.py
+++ optimum/tpu/fsdp_v2.py
@@ -82,8 +82,9 @@ def get_fsdp_training_args(model: PreTrainedModel) -> Dict:
     model_type = model.config.model_type
     matched_model = False
     if model_type == "gemma":
-        from .modeling_gemma import GemmaForCausalLM
         from transformers import GemmaForCausalLM as HFGemmaForCausalLLM
+
+        from .modeling_gemma import GemmaForCausalLM
 
         if isinstance(model, GemmaForCausalLM) or isinstance(model, HFGemmaForCausalLLM):
             logger = logging.get_logger(__name__)
@@ -95,8 +96,9 @@ def get_fsdp_training_args(model: PreTrainedModel) -> Dict:
             cls_to_wrap = "GemmaDecoderLayer"
             matched_model = True
     elif model_type == "llama":
-        from .modeling_llama import LlamaForCausalLM
         from transformers import LlamaForCausalLM as HFLlamaForCausalLLM
+
+        from .modeling_llama import LlamaForCausalLM
 
         if isinstance(model, LlamaForCausalLM) or isinstance(model, HFLlamaForCausalLLM):
             cls_to_wrap = "LlamaDecoderLayer"
