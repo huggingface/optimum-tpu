6c9348ce583af76825b56de768141eab3f2c3e6e
Alvaro Moran <alvaro.moran@huggingface.co>
2024-08-26 16:28:24 +0000

    feat(tgi): introduce AutoGenerator
    This is just a way to provide a factory class method to create
    Jetstream/Pytorch or Pytorch XLA generator.

---
+++ text-generation-inference/server/text_generation_server/auto_generator.py
@@ -0,0 +1,35 @@
+from .generator_base import Generator
+from .jetstream_pt_support import check
+
+
+class AutoGenerator:
+
+    @staticmethod
+    def from_pretrained(
+        model_path: str, revision: str, max_batch_size: int, max_sequence_length: int
+    ) -> Generator:
+        """Instantiate a Generator for TPU using Jetstream Pytorch or Pytorch/XLA.
+
+        Args:
+            model_path (`str`):
+                The path to a local model. This path must also contain a Tokenizer.
+            revision (`str`):
+                The revision of the model.
+            max_batch_size (`int`):
+                The maximum batch size.
+            max_sequence_length (`int`):
+                The maximum sequence length.
+
+        Returns:
+            A TpuGenerator.
+        """
+        if check(model_path):
+            from .jetstream_pt_support.generator import TpuGeneratorJetStream
+            return TpuGeneratorJetStream.from_pretrained(
+                model_path, revision=revision, max_batch_size=max_batch_size, max_sequence_length=max_sequence_length
+            )
+        else:
+            from .generator import TpuGenerator
+            return TpuGenerator.from_pretrained(
+                model_path, revision=revision, max_batch_size=max_batch_size, max_sequence_length=max_sequence_length
+            )
