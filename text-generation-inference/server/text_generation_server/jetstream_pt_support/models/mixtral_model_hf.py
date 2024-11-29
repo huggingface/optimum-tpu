54183d756c9e97dc33a2e9cde6425e612f5f9747
Alvaro Moran <alvaro.moran@huggingface.co>
2024-11-29 10:41:14 +0000

    review(mixtral): use properties in config to avoid aliasing ambiguity
    Instead of assigning separate variables for Jetstream's config class,
    properties are added, resulting in accessing the same data and avoiding
    ambiguity.

--- text-generation-inference/server/text_generation_server/jetstream_pt_support/models/mixtral_model_hf.py
+++ text-generation-inference/server/text_generation_server/jetstream_pt_support/models/mixtral_model_hf.py
@@ -1,26 +1,42 @@
-
 from jetstream_pt.third_party.mixtral import config as mixtral_config
 from jetstream_pt.third_party.mixtral.model import Transformer
 from transformers import GenerationConfig, GenerationMixin, MixtralConfig
 
 
 class MixtralConfigHf(MixtralConfig, mixtral_config.ModelArgs):
-    """This class is used to support both the HF MixtralConfig and the Jetstream Pytorch ModelArgs at the same time.
-    """
+    """This class is used to support both the HF MixtralConfig and the Jetstream Pytorch ModelArgs at the same time."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.block_size = self.max_position_embeddings
-        self.n_layer = self.num_hidden_layers
-        self.n_head = self.num_attention_heads
-        self.dim = self.hidden_size
-        self.n_local_heads = self.num_local_experts or self.num_attention_heads
-        self.num_activated_experts = self.num_experts_per_tok
         self.__post_init__()
 
+    @property
+    def block_size(self):
+        return self.max_position_embeddings
+
+    @property
+    def n_layer(self):
+        return self.num_hidden_layers
+
+    @property
+    def n_head(self):
+        return self.num_attention_heads
+
+    @property
+    def dim(self):
+        return self.hidden_size
+
+    @property
+    def n_local_heads(self):
+        return self.num_local_experts or self.num_attention_heads
+
+    @property
+    def num_activated_experts(self):
+        return self.num_experts_per_tok
+
+
 class MixtralModelHf(Transformer, GenerationMixin):
-    """Transformer module that uses HF MixtralConfig instead of Jetstream Pytorch MixtralConfig + device.
-    """
+    """Transformer module that uses HF MixtralConfig instead of Jetstream Pytorch MixtralConfig + device."""
 
     def __init__(
         self,
